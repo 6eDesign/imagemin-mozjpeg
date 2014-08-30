8527e638dd1219e9dc749880c17cf9fd787a5c00
Jonathan Greenemeier <jon@6eDesign.net>
2014-08-30 12:25:50 -0600

    Adding quality setting because wtf where is it?

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ var mozjpeg = require('mozjpeg').path;
  * @api public
  */
 
+var eligibleKeyValCommands = [ 
+	/* 
+		For more information on these options or to add more options to this script, see: 
+			https://github.com/mozilla/mozjpeg/blob/master/usage.txt
+	*/
+		'quality'
+		/*
+			Scale quantization tables to adjust image quality.
+			Quality is 0 (worst) to 100 (best); default is 75.
+			(See below for more info.)
+	 	*/
+]
 module.exports = function (opts) {
 	opts = opts || {};
 
@@ -23,6 +35,11 @@ module.exports = function (opts) {
 		var exec = new ExecBuffer();
 		var args = ['-copy', 'none'];
 
+		for(var i=0; i < eligibleKeyValCommands.length; ++i) { 
+			if(opts[eligibleKeyValCommands[i]]) 
+				args.push('-'+eligibleKeyValCommands[i]+' ' + opts[eligibleKeyValCommands[i]])
+		}
+		
 		if (opts.fastcrush) {
 			args.push('-fastcrush');
 		}
